29a7f2efc3df9c380a7a2dabf51d095e1fc980ac
abhishek-rabidas <avisec07@gmail.com>
2024-11-12 22:41:07 +0530

    dao setup

---
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/dao/BaseRepository.java
@@ -0,0 +1,10 @@
+package com.github.abhishek_rabidas.Hotel_Integration_API.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID>, QuerydslPredicateExecutor<T> {
+    T findByUuid(String uuid);
+}
---
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/dao/HotelRepository.java
@@ -0,0 +1,7 @@
+package com.github.abhishek_rabidas.Hotel_Integration_API.dao;
+
+import com.github.abhishek_rabidas.Hotel_Integration_API.models.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+}
