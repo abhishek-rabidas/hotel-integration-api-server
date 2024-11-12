ee31773a106265a1e88acf5c03dac07175c23709
abhishek-rabidas <avisec07@gmail.com>
2024-11-12 21:12:04 +0530

    project init

---
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/HotelIntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.abhishek_rabidas.Hotel_Integration_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelIntegrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelIntegrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/abhishek_rabidas/Hotel_Integration_API/HotelIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.abhishek_rabidas.Hotel_Integration_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelIntegrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
