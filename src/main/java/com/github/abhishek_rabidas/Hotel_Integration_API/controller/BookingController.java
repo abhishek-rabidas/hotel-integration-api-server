ad0df9b333f44081720ead1f39d90fa3e1f11cbe
abhishek-rabidas <avisec07@gmail.com>
2024-11-13 16:00:16 +0530

    booking controller

---
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/controller/BookingController.java
@@ -0,0 +1,30 @@
+package com.github.abhishek_rabidas.Hotel_Integration_API.controller;
+
+import com.github.abhishek_rabidas.Hotel_Integration_API.dto.BookingResponse;
+import com.github.abhishek_rabidas.Hotel_Integration_API.dto.CreateBookingRequest;
+import com.github.abhishek_rabidas.Hotel_Integration_API.service.BookingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/booking")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping
+    public void createBooking(@RequestBody CreateBookingRequest request) {
+        bookingService.createBooking(request);
+    }
+
+    @GetMapping("/list/user")
+    public List<BookingResponse> getAllBookingsByUser(@RequestParam String userId) {
+        return bookingService.getBookingsForUser(userId);
+    }
+
+}
