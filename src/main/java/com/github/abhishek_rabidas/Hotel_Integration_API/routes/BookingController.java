9ce2aa4f86507c8952d347ba1c88553f345e8574
abhishek-rabidas <avisec07@gmail.com>
2024-11-14 19:20:58 +0530

    routes safeguarding with privilege

--- src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/routes/BookingController.java
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/routes/BookingController.java
@@ -3,9 +3,14 @@ package com.github.abhishek_rabidas.Hotel_Integration_API.routes;
 import com.github.abhishek_rabidas.Hotel_Integration_API.dto.BookingResponse;
 import com.github.abhishek_rabidas.Hotel_Integration_API.dto.CreateBookingRequest;
 import com.github.abhishek_rabidas.Hotel_Integration_API.service.BookingService;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.github.abhishek_rabidas.Hotel_Integration_API.auth.HRMSAuthorization.Privileges.BOOKING_READ;
+import static com.github.abhishek_rabidas.Hotel_Integration_API.auth.HRMSAuthorization.Privileges.BOOKING_WRITE;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -18,26 +23,31 @@ public class BookingController {
     }
 
     @PostMapping
+    @Secured(BOOKING_WRITE)
     public void createBooking(@RequestBody CreateBookingRequest request) {
         bookingService.createBooking(request);
     }
 
     @GetMapping("/list/user")
+    @Secured(BOOKING_READ)
     public List<BookingResponse> getAllBookingsByUser(@RequestParam String userId) {
         return bookingService.getBookingsForUser(userId);
     }
 
     @PutMapping("/cancel/{id}")
+    @Secured(BOOKING_WRITE)
     public void cancelBooking(@PathVariable String id) {
         bookingService.cancelBooking(id);
     }
 
     @PostMapping("/checkIn/{id}")
+    @Secured(BOOKING_WRITE)
     public void checkIn(@PathVariable String id) {
         bookingService.checkInBooking(id);
     }
 
     @PostMapping("/checkout/{id}")
+    @Secured(BOOKING_WRITE)
     public void checkout(@PathVariable String id) {
         bookingService.cancelBooking(id);
     }
--- src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/routes/HotelController.java
+++ src/main/java/com/github/abhishek_rabidas/Hotel_Integration_API/routes/HotelController.java
@@ -7,7 +7,11 @@ import com.github.abhishek_rabidas.Hotel_Integration_API.service.HotelService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import static com.github.abhishek_rabidas.Hotel_Integration_API.auth.HRMSAuthorization.Privileges.HOTEL_READ;
+import static com.github.abhishek_rabidas.Hotel_Integration_API.auth.HRMSAuthorization.Privileges.HOTEL_WRITE;
 
 @RestController
 @RequestMapping("/api/hotel")
@@ -20,27 +24,32 @@ public class HotelController {
     }
 
     @PostMapping
+    @Secured(HOTEL_WRITE)
     public ResponseEntity<HotelResponse> createHotel(@RequestBody CreateHotelRequest createHotelRequest) {
         return ResponseEntity.ok(hotelService.createHotel(createHotelRequest));
     }
 
     @GetMapping
+    @Secured(HOTEL_READ)
     public ResponseEntity<PageResponse<HotelResponse>> getAllHotels(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         Pageable paging = PageRequest.of(page, size);
         return ResponseEntity.ok(hotelService.listHotels(paging));
     }
 
     @GetMapping("/{id}")
+    @Secured(HOTEL_READ)
     public ResponseEntity<HotelResponse> getHotel(@PathVariable String id) {
         return ResponseEntity.ok(hotelService.fetchHotelDetails(id));
     }
 
     @PutMapping("/deactivate/{id}")
+    @Secured(HOTEL_WRITE)
     public void archiveHotel(@PathVariable String id) {
         hotelService.changeHotelStatus(id, false);
     }
 
     @PutMapping("/activate/{id}")
+    @Secured(HOTEL_WRITE)
     public void makeHotelActive(@PathVariable String id) {
         hotelService.changeHotelStatus(id, true);
     }
